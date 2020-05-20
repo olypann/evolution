e3d19b9266a0fa33918975a0ec56c899f54f8aca
lessyat <panylessyat@gmail.com>
2020-05-20 19:37:17 +0200

    Pygame init

--- EVOLUTION.py
+++ EVOLUTION.py
@@ -1,8 +1,13 @@
+import pygame
+from pygame.locals import *
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from evolution_classes import World
+import seaborn as sns
 sns.set(style='darkgrid')
+
+SCREENRECT = Rect(0, 0, 640, 480)
+DAYS = 10
 
 
 def show_analytics(data):
@@ -14,13 +19,28 @@ def show_analytics(data):
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode(SCREENRECT.size)
     data = []
-    days = 7
     world = World()
-    for day in range(1, days+1):
+    running = True
+    print('day | population | average age')
+    print('-------------------------------')
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
         world.evolve()
-        data.append({'day': day, 'population': len(world.creatures)})
+        if world.days == DAYS:
+            running = False
+        total_age = 0
+        for creature in world.creatures:
+            total_age += world.days - creature.birthday
+        mean_age = total_age / len(world.creatures)
+        print('{:^3} | {:^10} | {:^11}'.format(world.days, len(world.creatures), mean_age))
+        data.append({'day': world.days, 'population': len(world.creatures), 'average age': mean_age})
     show_analytics(data)
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
--- evolution_classes.py
+++ evolution_classes.py
@@ -1,15 +1,19 @@
 class Creature:
-    def __init__(self):
+    def __init__(self, day):
         self.death_rate = 0
+        self.birthday = day
 
 
 class World:
     def __init__(self):
         self.birth_rate = 1
         self.creatures = []
+        self.days = 0
 
     def spontaneous_birth(self):
-        self.creatures.append(Creature())
+        self.creatures.append(Creature(day=self.days))
 
     def evolve(self):
+        self.days += 1
         self.spontaneous_birth()
+

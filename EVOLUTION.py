e091d108e9382fce84872011bc398fcec218a18b
lessyat <panylessyat@gmail.com>
2020-06-01 20:06:17 +0200

    work with sprites

--- EVOLUTION.py
+++ EVOLUTION.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from pygame.locals import *
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,6 +9,7 @@ sns.set(style='darkgrid')
 
 SCREENRECT = Rect(0, 0, 640, 480)
 DAYS = 10
+WHITE = (255, 255, 255)
 
 
 def show_analytics(data):
@@ -21,11 +23,12 @@ def show_analytics(data):
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREENRECT.size)
+    screen.fill(WHITE)
     data = []
     world = World()
-    running = True
     print('day | population | average age')
     print('-------------------------------')
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -38,9 +41,20 @@ def main():
             total_age += world.days - creature.birthday
         mean_age = total_age / len(world.creatures)
         print('{:^3} | {:^10} | {:^11}'.format(world.days, len(world.creatures), mean_age))
-        data.append({'day': world.days, 'population': len(world.creatures), 'average age': mean_age})
+        data.append({
+            'day': world.days,
+            'population': len(world.creatures),
+            'average age': mean_age,
+            'nusha population': len([c for c in world.creatures if type(c).__name__ == 'Nusha']),
+            'kopatich population': len([c for c in world.creatures if type(c).__name__ == 'Kopatich']),
+        })
+        screen.fill(WHITE)
+        world.creatures_group.draw(screen)
+        pygame.display.flip()
+        time.sleep(1)
     show_analytics(data)
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
--- evolution_classes.py
+++ evolution_classes.py
@@ -1,5 +1,20 @@
-class Creature:
+import os
+
+from pygame.locals import *
+import pygame
+import random
+import PIL
+
+
+SCREENRECT = Rect(0, 0, 640, 480)
+
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+class Creature(pygame.sprite.Sprite):
     def __init__(self, day):
+        super().__init__()
         self.death_rate = 0
         self.birthday = day
 
@@ -9,11 +24,46 @@ class World:
         self.birth_rate = 1
         self.creatures = []
         self.days = 0
+        self.creatures_group = pygame.sprite.Group()
 
     def spontaneous_birth(self):
-        self.creatures.append(Creature(day=self.days))
+        new_creature = Nusha(day=self.days)
+        self.creatures.append(new_creature)
+        self.creatures_group.add(new_creature)
+        new_creature = Kopatich(day=self.days)
+        self.creatures.append(new_creature)
+        self.creatures_group.add(new_creature)
 
     def evolve(self):
         self.days += 1
         self.spontaneous_birth()
+
+
+class Nusha(Creature):
+    def __init__(self, day):
+        super().__init__(day)
+        self.image = load_image('nusha.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, SCREENRECT.width-self.rect.width)
+        self.rect.y = random.randint(0, SCREENRECT.height-self.rect.height)
+
+
+class Kopatich(Creature):
+    def __init__(self, day):
+        super().__init__(day)
+        self.image = load_image('kopatich.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, SCREENRECT.width-self.rect.width)
+        self.rect.y = random.randint(0, SCREENRECT.height-self.rect.height)
+
+
+def load_image(file):
+    file = os.path.join(main_dir, 'data', file)
+    image = pygame.image.load(file)
+    image = image.convert_alpha()
+    return image
+
+
+
+
 
